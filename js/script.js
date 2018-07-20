ea192a32ad866754fe3fb4df65fc33b81f0cedff
TonyKioko <tonykioko384@gmail.com>
2018-07-20 21:28:16 +0300

    add initialize game function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 var scores, roundScore, activePlayer, diceRoll, gamePlaying;
+
+
+
+
+
+
+
+function initializeGame(){
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gamePlaying = true;
+  $("#score-0").text(0);
+  $("#score-1").text(0);
+  $("#current-0").text(0);
+  $("#current-1").text(0);
+  $("#name-0").text("Player 1");
+  $("#name-1").text("Player 2");
+  $(".gamer-0-section").removeClass("winner");
+  $(".gamer-1-section").removeClass("winner");
+  $(".gamer-0-section").removeClass("active");
+  $(".gamer-1-section").removeClass("active");
+  $(".gamer-0-section").addClass("active");
+
+}
