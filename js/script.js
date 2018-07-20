b7accd7ed91678d9b7419ecbac1c9d046fafde06
TonyKioko <tonykioko384@gmail.com>
2018-07-20 21:30:50 +0300

    roll dice complete

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
 var scores, roundScore, activePlayer, diceRoll, gamePlaying;
 
 
+initializeGame();
 
+$(document).ready(function(){
+  $(".btn-dice").click(function(){
+    if(gamePlaying){
+      diceRoll = Math.floor(Math.random()*6)+1;
+      console.log(diceRoll);
+      var diceShow = $(".dice");
+      diceShow.attr("src",'images/throw-' + diceRoll + '.png')
+      diceShow.show();
+      $("#current-"+activePlayer).text(diceRoll);
+      if (diceRoll !==1){
+        roundScore +=diceRoll;
+          // $("#score-"+activePlayer).text(roundScore);
+      }else {
+        nextGamer();
+      }
+    }
+
+    console.log(roundScore);
+  })
 
 
 
