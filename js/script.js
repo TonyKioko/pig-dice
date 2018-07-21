4ec76417f2b82d0d0c89ae28fc4e33d8920117e5
TonyKioko <tonykioko384@gmail.com>
2018-07-21 11:36:54 +0300

    roundscore holder box complete

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
       $("#current-" + activePlayer).text(diceRoll);
       if (diceRoll !== 1) {
         roundScore += diceRoll;
-        $("#roundscr-"+activePlayer).text(roundScore);
+        $("#roundscore-"+activePlayer).text(roundScore);
       } else {
         nextGamer();
       }
@@ -28,8 +28,8 @@ $(document).ready(function() {
     if (gamePlaying) {
       scores[activePlayer] += roundScore;
       $("#score-" + activePlayer).text(scores[activePlayer]);
-      $("#roundscr-0").text(0);
-      $("#roundscr-1").text(0);
+      $("#roundscore-0").text(0);
+      $("#roundscore-1").text(0);
       var finalScore = parseInt($(".final-score").val());
       var winningScore;
       if (finalScore) {
@@ -44,8 +44,9 @@ $(document).ready(function() {
         $(".gamer-" + activePlayer + "-section").addClass("winner");
         $(".gamer-" + activePlayer + "-section").removeClass("active");
         // $("#roundscr-"+activePlayer).text(0);
-        $("#roundscr-0").text(0);
-        $("#roundscr-1").text(0);
+        $("#roundscore-0").text(0);
+        $("#roundscore-1").text(0);
+        $(".final-score").val("")
         gamePlaying = false;
       } else {
         nextGamer();
@@ -69,8 +70,8 @@ var nextGamer = function() {
   // $(".player-0-panel").removeClass("active");
   // $(".player-1-panel").addClass("active");
   $(".dice").hide();
-  $("#roundscr-0").text(0);
-  $("#roundscr-1").text(0);
+  $("#roundscore-0").text(0);
+  $("#roundscore-1").text(0);
 }
 
 function initializeGame() {
@@ -82,8 +83,9 @@ function initializeGame() {
   $("#score-1").text(0);
   $("#current-0").text(0);
   $("#current-1").text(0);
-  $("#roundscr-0").text(0);
-  $("#roundscr-1").text(0);
+  $("#roundscore-0").text(0);
+  $("#roundscore-1").text(0);
+  $(".final-score").val("")
   $("#name-0").text("Player 1");
   $("#name-1").text("Player 2");
   $(".gamer-0-section").removeClass("winner");
